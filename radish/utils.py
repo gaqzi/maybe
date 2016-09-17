3d81fe55c2e745481dfcaeba6bea12066f74c9a5
Björn Andersson <ba@sanitarium.se>
2016-09-18 01:53:48 +0800

    Add TimeTaken to pretty-print elapsed times

--- radish/utils.py
+++ radish/utils.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def timer(kallable):
@@ -16,3 +16,107 @@ def timer(kallable):
     start_time = datetime.now()
 
     return kallable(), datetime.now() - start_time
+
+
+class TimeTaken(object):
+    def __init__(self, elapsed_time):
+        """Presentation class for :class:`datetime.timedelta`
+
+        Will provide pretty printed hour, minutes, and seconds of time
+        elapsed when printed/converted to a string.
+
+        Args:
+            elapsed_time (Union[timedelta, int, float]): How much time
+                has elapsed
+        """
+        if not isinstance(elapsed_time, timedelta):
+            elapsed_time = timedelta(seconds=elapsed_time)
+
+        self.elapsed_time = elapsed_time  # type: timedelta
+
+    def __str__(self):
+        """Elapsed time like: 1 hour, 23 minutes, and 2.3 seconds
+
+        Returns:
+            str: Pretty printed time information
+        """
+        hours, seconds = divmod(self.elapsed_time.total_seconds(), 3600)
+        minutes, seconds = divmod(seconds, 60)
+
+        return self._join_output(list(filter(lambda x: x, [
+            self._pluralize(int(hours), 'hour', 'hours'),
+            self._pluralize(int(minutes), 'minute', 'minutes'),
+            self._pluralize(round(seconds, 3), 'second', 'seconds')
+        ])))
+
+    __unicode__ = __str__
+
+    def __repr__(self):
+        return 'TimeTaken(elapsed_time={})'.format(self.elapsed_time.__repr__())
+
+    def __add__(self, other):
+        """Adds the time from another object and returns a new instance
+
+        Args:
+            other (Union[TimeTaken, int, float, timedelta]): The object to add
+
+        Returns:
+            TimeTaken: A new instance of TimeTaken with the calculation done
+
+        Raises:
+            TypeError: For others that can't be used by timedelta
+        """
+        if hasattr(other, 'elapsed_time'):
+            return TimeTaken(self.elapsed_time + other.elapsed_time)
+        else:
+            if isinstance(other, (int, float)):
+                other = timedelta(seconds=other)
+
+        return TimeTaken(self.elapsed_time + other)
+
+    def __sub__(self, other):
+        """Adds the time from another object and returns a new instance
+
+        Args:
+            other (Union[TimeTaken, int, float, timedelta]): The object to add
+
+        Returns:
+            TimeTaken: A new instance of TimeTaken with the calculation done
+
+        Raises:
+            TypeError: For others that can't be used by timedelta
+        """
+        if hasattr(other, 'elapsed_time'):
+            return TimeTaken(self.elapsed_time - other.elapsed_time)
+        else:
+            if isinstance(other, (int, float)):
+                other = timedelta(seconds=other)
+
+            return TimeTaken(self.elapsed_time - other)
+
+    def __eq__(self, other):
+        if hasattr(other, 'elapsed_time'):
+            return self.elapsed_time == other.elapsed_time
+        else:
+            if isinstance(other, (int, float)):
+                other = timedelta(seconds=other)
+
+            return self.elapsed_time == other
+
+    def _pluralize(self, value, singular, plural):
+        if value:
+            return '{} {}'.format(
+                value,
+                plural if value > 1 else singular
+            )
+
+    def _join_output(self, times):
+        if len(times) > 2:
+            return '{}, and {}'.format(
+                ', '.join(times[0:-1]),
+                times[-1]
+            )
+        elif len(times) == 2:
+            return ' and '.join(times)
+        else:
+            return ''.join(times)
---
+++ tests/test_utils.py
@@ -0,0 +1,71 @@
+from time import sleep
+
+import pytest
+
+from radish.utils import TimeTaken
+
+
+class TestTimeTaken(object):
+    class TestStr(object):
+        def test_taken_more_than_an_hour(self):
+            assert str(TimeTaken(3662.19)) == '1 hour, 1 minute, and 2.19 seconds'
+
+        def test_taken_exactly_on_the_hour(self):
+            assert str(TimeTaken(3600)) == '1 hour'
+            assert str(TimeTaken(7200)) == '2 hours'
+
+        def test_taken_less_than_an_hour(self):
+            assert str(TimeTaken(150.40)) == '2 minutes and 30.4 seconds'
+
+        def test_taken_exactly_on_the_minute(self):
+            assert str(TimeTaken(60.0)) == '1 minute'
+            assert str(TimeTaken(120.0)) == '2 minutes'
+
+        def test_taken_less_than_a_minute(self):
+            assert str(TimeTaken(30.5)) == '30.5 seconds'
+            assert str(TimeTaken(1)) == '1.0 second'
+            assert str(TimeTaken(0.1)) == '0.1 second'
+
+        def test__str__and__unicode__returns_the_same(self):
+            time = TimeTaken(30.5)
+
+            assert time.__str__() == time.__unicode__()
+
+    class TestRepr(object):
+        def test_output_info_about_timedelta(self):
+            assert TimeTaken(0).__repr__() == 'TimeTaken(elapsed_time=datetime.timedelta(0))'
+
+    class TestAddAndRemoveTimeTaken(object):
+        def test_add_two_time_taken(self):
+            assert (TimeTaken(1) + TimeTaken(1)).elapsed_time.total_seconds() == 2.0
+
+        def test_adding_other_objects_gets_passed_down_to_timedelta(self):
+            with pytest.raises(TypeError, message='asdf'):
+                TimeTaken(1) + 'm000'
+
+        def test_adding_int_and_float_gets_converted_to_timedelta_before_passed_on(self):
+            assert (TimeTaken(1) + 1).elapsed_time.total_seconds() == 2.0
+            assert (TimeTaken(1) + 1.0).elapsed_time.total_seconds() == 2.0
+
+        def test_subtract_time_taken(self):
+            assert (TimeTaken(2) - TimeTaken(1)).elapsed_time.total_seconds() == 1.0
+
+        def test_subtracting_other_objects_gets_passed_down_to_timedelta(self):
+            with pytest.raises(TypeError, message='asdf'):
+                TimeTaken(1) - 'm000'
+
+        def test_subtracting_int_and_float_gets_converted_to_timedelta_before_passed_on(self):
+            assert (TimeTaken(2) - 1).elapsed_time.total_seconds() == 1.0
+            assert (TimeTaken(2) - 1.0).elapsed_time.total_seconds() == 1.0
+
+    class TestEquality(object):
+        def test_equal_if_the_same_elapsed_time(self):
+            assert TimeTaken(1) == TimeTaken(1)
+
+        def test_equalling_other_objects_gets_passed_down_to_timedelta(self):
+            assert TimeTaken(1) != 'm000'
+
+        def test_equal_successfully_against_integers_and_floats(self):
+            assert TimeTaken(2) == 2
+            assert TimeTaken(2.3) != 2
+            assert TimeTaken(2.3) == 2.3
